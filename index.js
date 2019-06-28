20f2a13b239558fb77552b19899d423e224c5294
Galaxy97 <atnyehin@gmail.com>
2019-06-28 13:10:22 +0300

    test GET POST request

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
-app.set("view engine","ejs");
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
 
 data_s = ["hello", "my", "dear", "friend"];
 
-app.get("/", (req, res) => res.render("index",{data:data_s}));
+app.get("/", (req, res) => res.render("index", { data: data_s }));
+
+app.get("/about/:id", (req, res) => {
+  res.send(req.params.id);
+});
+app.get("/about", (req, res) => {
+  res.send(req.query.some);
+});
+app.get("/test", function(req, res) {
+  res.render("test");
+});
+app.post("/test", function(req, res) {
+  res.send("POST request " + req.body.text);
+});
 
 app.listen(port, () => console.log("Example app listening on port port!"));
